f171f2971afb9ff9a762233826ee841991013a81
Edgar Abgaryan <garinich.7@gmail.com>
2023-02-18 00:28:34 +0400

    mru

--- src/cacheReplacementStrategies/index.ts
+++ src/cacheReplacementStrategies/index.ts
@@ -1,9 +1,11 @@
 import type { CacheReplacementStrategy } from "../types";
 import { lru } from "./lru";
+import { mru } from "./mru";
 
 
 const allStrategies: Record<string, CacheReplacementStrategy> = {
   lru,
+  mru,
 };
 
 export default allStrategies;
---
+++ src/cacheReplacementStrategies/mru.ts
@@ -0,0 +1,15 @@
+import type { CacheReplacementStrategy, Hit } from "../types";
+
+export const mru: CacheReplacementStrategy = <T extends {hits: Hit[]} = {hits: Hit[]}>(items: T[]): T[] => {
+  const asArr = [...items]
+    .sort((a, b) => {
+      const aIndex = a.hits.at(-1)?.index || 0;
+      const bIndex = b.hits.at(-1)?.index || 0;
+
+      return bIndex - aIndex;
+    });
+
+  return asArr.slice(0, 1);
+};
+
+export default mru;
