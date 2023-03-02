70f90c3fa3e8f8f71126a132fc949b1441683eb2
Edgar Abgaryan <garinich.7@gmail.com>
2023-03-02 23:51:28 +0400

    article solutionExample.js 8

--- article/solutionExample.js
+++ article/solutionExample.js
@@ -4,15 +4,23 @@ export const withMemo = (
     getKey = (arg) => arg,
     getCacheStore = () => new Map(),
     cacheRejectedPromise = false,
+    ttl,
   } = {},
 ) => {
   const createSubCache = () => ({
     subCaches: getCacheStore(),
     result: null,
     isCached: false,
+    invalidationTimeoutId: null,
   });
 
   const rootCache = createSubCache();
+
+  const invalidateByCache = (theCache) => {
+    theCache.isCached = false;
+    theCache.result = null;
+    clearTimeout(theCache.invalidationTimeoutId);
+  };
 
   return (...args) => {
     let currentCache = rootCache;
@@ -29,13 +37,19 @@ export const withMemo = (
     if (!currentCache.isCached) {
       currentCache.result = originFn(...args);
       currentCache.isCached = true;
+
+      if (ttl != null) {
+        currentCache.invalidationTimeoutId = setTimeout(
+          () => invalidateByCache(currentCache),
+          ttl,
+        );
+      }
     }
 
     if (!cacheRejectedPromise) {
-      Promise.resolve(currentCache.result).catch(() => {
-        currentCache.isCached = false;
-        currentCache.result = null;
-      });
+      Promise.resolve(currentCache.result).catch(() =>
+        invalidateByCache(currentCache),
+      );
     }
 
     return currentCache.result;
