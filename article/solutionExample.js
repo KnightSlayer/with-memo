1a5809cb1bd769e3e49cb5fe5abccb4354fbe2ce
Edgar Abgaryan <garinich.7@gmail.com>
2023-03-02 23:53:40 +0400

    article solutionExample.js 9

--- article/solutionExample.js
+++ article/solutionExample.js
@@ -1,3 +1,12 @@
+const cacheReplacementStrategies = {
+  lru: (itemsSet) => {
+    const asArray = Array.from(itemsSet)
+      .sort((a, b) => a.hits.at(-1) - b.hits.at(-1));
+    
+    return asArray.slice(0, 1);
+  },
+};
+
 export const withMemo = (
   originFn,
   {
@@ -5,21 +14,32 @@ export const withMemo = (
     getCacheStore = () => new Map(),
     cacheRejectedPromise = false,
     ttl,
+    cacheReplacementPolicy = null,
   } = {},
 ) => {
+  if (cacheReplacementPolicy && !cacheReplacementPolicy.maxSize) {
+    throw new Error("maxSize is mandatory for cacheReplacementPolicy");
+  }
+
   const createSubCache = () => ({
     subCaches: getCacheStore(),
     result: null,
     isCached: false,
     invalidationTimeoutId: null,
+    hits: [],
   });
 
   const rootCache = createSubCache();
 
+  const allCacheRecords = new Set();
+
   const invalidateByCache = (theCache) => {
+    clearTimeout(theCache.invalidationTimeoutId);
     theCache.isCached = false;
     theCache.result = null;
-    clearTimeout(theCache.invalidationTimeoutId);
+    theCache.invalidationTimeoutId = null;
+    theCache.hits = [];
+    allCacheRecords.delete(theCache);
   };
 
   return (...args) => {
@@ -35,6 +55,17 @@ export const withMemo = (
     }
 
     if (!currentCache.isCached) {
+      if (cacheReplacementPolicy) {
+        const {
+          maxSize,
+          strategy = cacheReplacementStrategies.lru,
+        } = cacheReplacementPolicy;
+        if (allCacheRecords.size >= maxSize) {
+          const cachesToReplace = strategy(allCacheRecords);
+          cachesToReplace.forEach(invalidateByCache);
+        }
+      }
+
       currentCache.result = originFn(...args);
       currentCache.isCached = true;
 
@@ -44,7 +75,9 @@ export const withMemo = (
           ttl,
         );
       }
+      allCacheRecords.add(currentCache);
     }
+    currentCache.hits.push(+new Date());
 
     if (!cacheRejectedPromise) {
       Promise.resolve(currentCache.result).catch(() =>
