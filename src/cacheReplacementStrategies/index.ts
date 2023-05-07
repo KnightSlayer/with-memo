31bd564c0d6ec2c02575750a927e3447a8f90dbd
Edgar Abgaryan <garinich.7@gmail.com>
2023-05-07 10:25:00 +0600

    1.0.0: add lfu into allStrategies

--- src/cacheReplacementStrategies/index.ts
+++ src/cacheReplacementStrategies/index.ts
@@ -1,11 +1,13 @@
 import type { CacheReplacementStrategy } from "../types";
 import { lru } from "./lru";
 import { mru } from "./mru";
+import { lfu } from "./lfu";
 
 
 const allStrategies: Record<string, CacheReplacementStrategy> = {
   lru,
   mru,
+  lfu,
 };
 
 export default allStrategies;
